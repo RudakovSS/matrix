d0bd33665ce2ccfac85cae32520eb53f958f9e89
RudakovSS <Stefan.rudakov@gmail.com>
2024-09-20 19:13:45 +0300

    Add files via upload

---
+++ matrix.py
@@ -0,0 +1,9 @@
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        matrix.append([])
+        for j in range(m):
+            matrix[i].append(value)
+    return matrix
+matrix = get_matrix(2,2, 10)
+print(matrix)
