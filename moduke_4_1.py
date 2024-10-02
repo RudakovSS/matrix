7216c4603d252aa63bf773485f51a31a4ff5131c
RudakovSS <Stefan.rudakov@gmail.com>
2024-10-02 20:03:38 +0300

    Add files via upload

---
+++ moduke_4_1.py
@@ -0,0 +1,8 @@
+
+from true_math import divide
+result_1 = divide(3, 4)
+result_2 = divide(3, 0)
+from fake_math import divide
+result_3 = divide(2, 3)
+result_4 = divide(4, 0)
+print(result_1, result_2, result_3, result_4)
