38bf9ce2dd6e49feafdd14a1bfa2088861f5ee66
RudakovSS <Stefan.rudakov@gmail.com>
2024-10-04 20:01:22 +0300

    Add files via upload

---
+++ module5.2.py
@@ -0,0 +1,25 @@
+
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print("Такого этажа не существует")
+        else:
+            for i in range(1, new_floor + 1):
+                print(i)
+    def __len__(self):
+        return self.number_of_floors
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей:{self.number_of_floors}'
+my_house = House('Эльбрус', 20)
+my_house.go_to(14)
+my_house.go_to(45)
+print(my_house)
+print(len(my_house))
+
+
+
+
+
