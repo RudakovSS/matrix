5d3a63613a1300854dc5813482fc7c3ed0826f93
RudakovSS <Stefan.rudakov@gmail.com>
2024-10-25 19:03:07 +0300

    Add files via upload

---
+++ module7_4.py
@@ -0,0 +1,21 @@
+# print( 'Привет, ' + 'мир!')
+# print( 'Меня зовут %(name)s, мне %(year)s' % {'name': 'Денис", 'year": 14})
+# 3 print('Я учусь в {title}fpostfix} {title}'. format(title='Урбан', postfix='-university'))
+# print(f' {"Urban"} это лучший университет! '))
+
+
+print("В команде Мастера кода участников: %s ! " % '5')
+print('Итого сегодня в командах участников: %s и %s' %(5, 6))
+print()
+print('Команда Волшебники данных решила задач: {}'.format('42'))
+print('Волшебники данных решили задачи за {} с !'.format('18015.2'))
+print()
+score_1 = 40
+score_2 = 42
+print(f'Команды решили {score_1} и {score_2} задач')
+challenge_result = 'победа команды Мастера кода'
+print(f'Результат битвы: {challenge_result}')
+tasks_total = 82
+time_avg = 350.4
+print(f'Сегодня было решено {tasks_total} задач, в среднем по {time_avg} секунды на задачу!')
+
